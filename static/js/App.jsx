32f1a8d3c7b956f66d2a6265109ab70f2f009f57
brandon05 <brandonsanchez05@gmail.com>
2017-10-20 14:27:11 -0400

    added background image and css

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
+import HeaderBackgroundImage from '../images/header.jpg';
+
+require('../css/fullstack.css');
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  addHeaderImg() {
+    let headerBg = new Image();
+    headerBg.src = HeaderBackgroundImage;
+  }
+
   render () {
-    return <p>Hello</p>
+    return (
+      <PageHeader>
+      <div className='header-image'>
+        {this.addHeaderImg()}
+      </div>
+      <div className='header-contents'>
+        <Hello name='Karlita' />
+      </div>
+      </PageHeader>
+    );
   }
 }
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: __dirname + '/js/index.jsx',
@@ -15,9 +16,23 @@ const config = {
         test: /\.jsx?/,
         exclude: /node_modules/,
         use: 'babel-loader'
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      },
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ]
 };
 
 module.exports = config;
